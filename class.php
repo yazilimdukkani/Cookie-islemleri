3dada341271511c29d43d89ef19fe07e5625fbc4
Baha Aktaş <133813921+yazilimdukkani@users.noreply.github.com>
2023-05-24 11:46:42 +0300

    class.php

--- class.php
+++ class.php
@@ -16,7 +16,7 @@ public function __construct(){
     $this->db->exec("SET NAMES utf8");
 
 */
-$this->db= new PDO('mysql:host=localhost; dbname=klasproje','root','flamenko86');
+$this->db= new PDO('mysql:host=localhost; dbname=klasproje','root','');
 $this->db->exec("SET NAMES utf8");
 
 }
